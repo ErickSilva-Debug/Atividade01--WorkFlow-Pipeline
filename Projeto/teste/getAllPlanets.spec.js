056a34b59d92413228457d02deb20eac9eb11642
ErickSA194 <erick.a@iesb.edu.br>
2024-10-31 09:44:46 -0300

    projeto finalizado

---
+++ Projeto/teste/getAllPlanets.spec.js
@@ -0,0 +1,10 @@
+// tests/getAllPlanets.spec.js
+const request = require("supertest");
+const app = require("../App");
+
+describe("Teste para obter todos os planetas", () => {
+  test("GET /api/planets - Deve retornar todos os planetas", async () => {
+    const response = await request(app).get("/api/planets");
+    expect(response.body.length).toBeGreaterThan(0);
+  });
+});
---
+++ Projeto/teste/getPlanetNotFound.spec.js
@@ -0,0 +1,11 @@
+// tests/getPlanetNotFound.spec.js
+const request = require("supertest");
+const app = require("../App");
+
+describe("Teste para quando planeta não existe", () => {
+  test("GET /api/planets/NonExistentPlanet - Deve retornar status 404", async () => {
+    const response = await request(app).get("/api/planets/NonExistentPlanet");
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe("Planet not found");
+  });
+});
